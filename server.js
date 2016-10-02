72a0d8a6f87b1606fc625ccfe4ab2d518840b2cb
sanattaori <sanattaori3@gmail.com>
2016-10-02 15:20:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,17 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('artical-one', function (req, res){
+app.get('/artical-one', function (req, res){
     res.send('Artical one requested');
     
 });
 
-app.get('artical-two', function (req, res){
+app.get('/artical-two', function (req, res){
     res.send('Artical two requested');
     
 });
 
-app.get('artical-three', function (req, res){
+app.get('/artical-three', function (req, res){
     res.send('Artical three requested');
     
 });
