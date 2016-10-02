4d5e8b481020bf6f77ac80a309617ff9d8ab3ef2
sanattaori <sanattaori3@gmail.com>
2016-10-02 16:15:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -38,10 +38,7 @@ var htmlTemplate = `
 <head>
 	<title>Article one</title>
 	<meta name ="viewport" content ="width=device-width,initial-scale=1">
-	<style>
-	    
-	    }
-	</style>
+	
 </head>
 <body>
 <div class ="container">
