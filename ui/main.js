38ddad18dc12aa40761b7ef8c6ad2b0e8f9879f5
Sanat Taori <sanattaori3@gmail.com>
2016-10-26 22:04:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,25 @@
 console.log('Loaded!');
+
+//move img
+var img = document.getElementById('madi');
+
+var marginLeft = 0;
+
+function moveRight(){
+	marginLeft = marginLeft +1;
+	img.style.marginLeft = marginLeft + 'px';
+}
+
+img.onclick = function() {
+	var interval = setInterval(moveRight,50);
+
+};
+
+var counter = 0;
+var button = document.getElementById('counter');
+button.onclick = function() {
+
+	counter = counter + 1;
+	var span = document.getElementById('count');
+	span.innerHTML = counter.toString();
+};
