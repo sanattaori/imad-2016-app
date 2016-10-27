8b6a0ac189fd20586068c36ae9c5e114cc5ab5d0
Sanat Taori <sanattaori3@gmail.com>
2016-10-28 00:31:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,12 +4,21 @@ setTimeout(function(){
  }, 5000);
 
 
-
+//counter
 var counter = 0;
 var button = document.getElementById('counter');
 button.onclick = function() {
+    
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function() {
+        if (request.readyState ===XMLHttpRequest.done){
+            if(request.status === 200) {
+                var counter = request.responseText;
+            }
+        }
+    };
+    request.open('GET ','http://http://sanattaori.imad.hasura-app.io/counter',true);
+    request.send(null);
 
-	counter = counter + 1;
-	var span = document.getElementById('count');
-	span.innerHTML = counter.toString();
 };
