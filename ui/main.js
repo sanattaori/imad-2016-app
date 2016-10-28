5ead9a4f9d23c7a96b0521de0830a5ce77992a4c
Sanat Taori <sanattaori3@gmail.com>
2016-10-28 22:30:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -33,7 +33,7 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     var request = new XMLHttpRequest();
-    
+      
     request.onreadystatechange = function() {
         if (request.readyState ===XMLHttpRequest.DONE){
             if(request.status === 200) {
