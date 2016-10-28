3d82c2f0396bee165dde8c567f5c2fc4963ed6da
Sanat Taori <sanattaori3@gmail.com>
2016-10-28 20:25:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,7 +21,7 @@ button.onclick = function() {
             }
         }
     };
-    request.open('GET ','http://http://sanattaori.imad.hasura-app.io/counter',true);
+    request.open('GET','http://sanattaori.imad.hasura-app.io/counter',true);
     request.send(null);
 
 };
