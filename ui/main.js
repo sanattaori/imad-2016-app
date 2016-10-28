f04e2135af851d43f2fb0eaf573d49caeefeaaf4
Sanat Taori <sanattaori3@gmail.com>
2016-10-28 22:50:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,8 +43,8 @@ submit.onclick = function(){
     for (var i=0; i<names.length; i++) {
         list+='<li>' + names[i] + '</li>'
     }
-    var ui = document.getElementById('namelist');
-    ui.innerHTML = list;
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
             }
         }
     };
