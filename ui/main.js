20243988331044e859c13640b49a6e6f67508514
Sanat Taori <sanattaori3@gmail.com>
2016-10-28 21:00:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,7 +34,7 @@ var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     
     var names = ['name1', 'name2', 'name3'];
-    var list='';
+    var list ='';
     for (var i=0; i<names.length; i++) {
         list+='<li>' + names[i] + '</li>'
     }
