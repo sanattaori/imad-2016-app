8eadd7dbddb8d05e0d205d0016d6899a73e1663b
Sanat Taori <sanattaori3@gmail.com>
2016-10-28 21:56:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -51,7 +51,7 @@ submit.onclick = function(){
     request.open('GET','http://sanattaori.imad.hasura-app.io/submit-name?name' + name,true);
     request.send(null);
     
-    var name = ['name1', 'name2', 'name3'];
+    var names = ['name1', 'name2', 'name3'];
     var list ='';
     for (var i=0; i<names.length; i++) {
         list+='<li>' + names[i] + '</li>'
