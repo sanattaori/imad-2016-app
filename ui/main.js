54b79e14b8d500fb28d8058586d8ab9fa9f5444f
Sanat Taori <sanattaori3@gmail.com>
2016-11-05 23:39:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,6 +9,13 @@ function onSignIn(googleUser) {
         console.log("Image URL: " + profile.getImageUrl());
 }
 
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
+
 
 $('.madi2').hover(function () {
   $(this).addClass('magictime vanishIn');
