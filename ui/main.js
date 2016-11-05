7a283a6033de00ccbf2336602e6f1d4e982d7d29
Sanat Taori <sanattaori3@gmail.com>
2016-11-06 00:02:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -68,6 +68,7 @@ submit.onclick = function(){
             }
         }
     };
+    
     var nameInput = document.getElementById('name');
     var name = nameInput.value;
     request.open('GET','http://sanattaori.imad.hasura-app.io/submit-name?name=' + name,true);
